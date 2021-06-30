a4c0e810c20616e88c58331b6364d9286b2ce673
Theodore Elikem Attigah <elikemteddy@gmail.com>
2021-06-30 16:24:53 +0000

    Implement promised based map function

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Works similar to Array.prototype.map but executes each iteration of the array asynchronously.
+ * This allows the execution to yield to other tasks thereby not blocking the event loop.
+ * @param {Array} array
+ * @param {Function} callback
+ * @returns A new array populated with the results of the callback function
+ */
+function map(array, callback) {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(array))
+      reject(new TypeError("Invalid argument. Must be an array"));
+    const newArray = [];
+    function help(index) {
+      if (index < array.length) {
+        Promise.resolve(callback(array[index], index))
+          .then((result) => {
+            newArray.push(result);
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      } else {
+        resolve(newArray);
+        return;
+      }
+      setImmediate(help.bind(null, index + 1));
+    }
+    help(0);
+  });
+}
+
+module.exports = { map };
