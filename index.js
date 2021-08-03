4cec99521c2fd5a14226ba50870a71046f305244
Theodore Elikem Attigah <elikemteddy@gmail.com>
2021-08-03 15:43:03 +0000

    Implement reduce function

--- index.js
+++ index.js
@@ -5,43 +5,84 @@
  * @param {Function} callback
  * @returns A new array populated with the results of the callback function
  */
-function map(array, callback) {
+function map(array, callback, thisArg) {
   return new Promise((resolve, reject) => {
     if (!Array.isArray(array))
       reject(new TypeError("Invalid argument. Must be an array"));
     const newArray = [];
     function help(index) {
       if (index < array.length) {
-        Promise.resolve(callback(array[index], index))
+        Promise.resolve(callback.call(thisArg, array[index], index, array))
           .then((result) => {
             newArray.push(result);
+            setImmediate(help.bind(null, index + 1));
           })
           .catch((error) => {
-            callback(error);
-            return;
+            reject(error);
           });
       } else {
         resolve(newArray);
-        return;
       }
-      setImmediate(help.bind(null, index + 1));
     }
     help(0);
   });
 }
 
-function forEach(array, callback) {
-  if (!Array.isArray(array))
-    reject(new TypeError("Invalid argument. Must be an array"));
-  function help(index) {
-    if (index < array.length) {
-      callback(array[index], index);
-    } else {
-      return;
+function forEach(array, callback, thisArg) {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(array))
+      reject(new TypeError("Invalid argument. Must be an array"));
+    function help(index) {
+      if (index < array.length) {
+        Promise.resolve(callback.call(thisArg, array[index], index, array))
+          .then((result) => {
+            setImmediate(help.bind(null, index + 1));
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      } else {
+        resolve(undefined);
+        // return;
+      }
     }
-    setImmediate(help.bind(null, index + 1));
-  }
-  help(0);
+    help(0);
+  });
 }
 
-module.exports = { map, forEach };
+function reduce(array, callback, initialValue) {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(array))
+      reject(new TypeError("Invalid argument. Must be an array"));
+    if (
+      (initialValue == undefined || initialValue == null) &&
+      array.length === 0
+    )
+      reject(new TypeError("Array must not be empty"));
+
+    let accumulator =
+      initialValue == undefined || initialValue == null
+        ? array[0]
+        : initialValue;
+    function help(index) {
+      if (index < array.length) {
+        Promise.resolve(callback(accumulator, array[index], index, array))
+          .then((result) => {
+            accumulator = result;
+            setImmediate(help.bind(null, index + 1));
+          })
+          .catch((error) => {
+            reject(error);
+            return;
+          });
+      } else {
+        resolve(accumulator);
+        return;
+      }
+    }
+    if (initialValue != undefined || initialValue != null) help(0);
+    else help(1);
+  });
+}
+
+module.exports = { map, forEach, reduce };
